0759ef04bd5a327d98a639455c91d725d7a333a6
shinythinking <shinythink@gmail.com>
2025-11-28 21:15:23 +0900

    feat: model 추가
    * Game 관련
    * Socket 관련

--- app/src/main/java/com/shinythinking/applepulser_android/domain/model/GameResult.kt
+++
@@ -1,7 +0,0 @@
-package com.shinythinking.applepulser_android.domain.model
-
-data class GameResult(
-    val rankings: List<PlayerResult>,
-    val gameMode: GameMode,
-    val gameDuration: Int
-)
--- app/src/main/java/com/shinythinking/applepulser_android/domain/model/PlayerResult.kt
+++ app/src/main/java/com/shinythinking/applepulser_android/domain/model/PlayerResult.kt
@@ -3,7 +3,10 @@ package com.shinythinking.applepulser_android.domain.model
 data class PlayerResult(
     val player: Player,
     val rank: Int,
+    val score: Int,
+    val workoutTime: Int,
     val averageBpm: Int,
-    val peakBpm: Int,
-    val timeInRange: Int
+    val maxBpm: Int,
+    val minBpm: Int,
+    val timeInZone: Int
 )
