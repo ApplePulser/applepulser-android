af71de3b2b7ae9d38e1fff93809ea0041c05be5c
shinythinking <shinythink@gmail.com>
2025-12-12 17:12:09 +0900

    feat: Websocket 변경에 따른 Event model 변경

--- app/src/main/java/com/shinythinking/applepulser_android/domain/model/event/GameEvent.kt
+++ app/src/main/java/com/shinythinking/applepulser_android/domain/model/event/GameEvent.kt
@@ -1,12 +1,11 @@
 package com.shinythinking.applepulser_android.domain.model.event
 
-import com.shinythinking.applepulser_android.domain.model.GameMode
 import com.shinythinking.applepulser_android.domain.model.Player
 import com.shinythinking.applepulser_android.domain.model.PlayerResult
 
 sealed interface GameEvent {
-    data class HeartbeatUpdate(val players: List<Player>, val timestamp: String) : GameEvent
-    data class GameEnded(val reason: String, val endedAt: String) : GameEvent
-    data class GameResultReceived(val rankings: List<PlayerResult>, val mode: GameMode) : GameEvent
-    data class Error(val message: String) : GameEvent
+    data class HeartbeatUpdate(val players: List<Player>) : GameEvent
+    data class GameEnded(val results: List<PlayerResult>) : GameEvent
+    data class PlayerLeft(val playerId: String, val nickname: String) : GameEvent
+    data class ErrorDelivered(val message: String) : GameEvent
 }
--- app/src/main/java/com/shinythinking/applepulser_android/domain/model/event/RoomEvent.kt
+++ app/src/main/java/com/shinythinking/applepulser_android/domain/model/event/RoomEvent.kt
@@ -1,13 +1,12 @@
 package com.shinythinking.applepulser_android.domain.model.event
 
-import com.shinythinking.applepulser_android.domain.model.GameMode
+import com.shinythinking.applepulser_android.domain.model.GameSetting
 import com.shinythinking.applepulser_android.domain.model.Player
 
 sealed interface RoomEvent {
+    data class PlayerReady(val playerId: String, val isReady: Boolean) : RoomEvent
+    data class GameStarted(val gameSetting: GameSetting) : RoomEvent
     data class PlayerJoined(val player: Player, val currentCount: Int) : RoomEvent
-    data class PlayerLeft(val playerId: String, val nickname: String, val currentCount: Int) :
-        RoomEvent
-
-    data class GameStarted(val mode: GameMode, val startedAt: String) : RoomEvent
-    data class Error(val message: String) : RoomEvent
+    data class PlayerLeft(val playerId: String, val nickname: String) : RoomEvent
+    data class ErrorDelivered(val message: String) : RoomEvent
 }
